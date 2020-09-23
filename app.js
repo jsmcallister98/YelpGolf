6d980d4418407d62f6211d17ccf9212ccca8ed3d
jsmcallister98 <jakemcallister@outlook.com>
2020-09-22 23:34:23 -0700

    Enabled functionality with MongoDB

--- app.js
+++ app.js
@@ -1,40 +1,84 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/yelp_golf", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connected to DB!'))
+  .catch(error => console.log(error.message));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-let campgrounds = [
-    {name: "Pebble Beach", image: "https://www.pebblebeach.com/content/uploads/20160811-cam-timeout.jpg"},
-    {name: "Pasatiempo", image: "https://www.pasatiempo.com/images/uploads/newhome1.png"},
-    {name: "Half Moon Bay", image: "https://www.visithalfmoonbay.org/sites/default/files/inline-images/Old%20Course_Ritz_photo1.jpg"},
-    {name: "Pebble Beach", image: "https://www.pebblebeach.com/content/uploads/20160811-cam-timeout.jpg"},
-    {name: "Pasatiempo", image: "https://www.pasatiempo.com/images/uploads/newhome1.png"},
-    {name: "Half Moon Bay", image: "https://www.visithalfmoonbay.org/sites/default/files/inline-images/Old%20Course_Ritz_photo1.jpg"},
-    {name: "Pebble Beach", image: "https://www.pebblebeach.com/content/uploads/20160811-cam-timeout.jpg"},
-    {name: "Pasatiempo", image: "https://www.pasatiempo.com/images/uploads/newhome1.png"},
-    {name: "Half Moon Bay", image: "https://www.visithalfmoonbay.org/sites/default/files/inline-images/Old%20Course_Ritz_photo1.jpg"}
-]
+// Schema setup
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+const campground = mongoose.model("Campground", campgroundSchema);
+
+// campground.create(    
+//     {
+//         name: "Pebble Beach", 
+//         image: "https://www.pebblebeach.com/content/uploads/20160811-cam-timeout.jpg",
+//         description: "The greatest public golf course on earth"
+//     },function(err, campground){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log("New Course: ");
+//             console.log(campground);
+//         }
+//     });
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds:campgrounds});
+    // get all courses from db
+    campground.find({}, function(err, campgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("index", {campgrounds:campgrounds});
+        }
+    });
 });
 
 app.post("/campgrounds", function(req, res){
     const name = req.body.name;
     const image = req.body.image;
-    const newCampground = {name: name, image: image}
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    const desc = req.body.description;
+    const newCampground = {name: name, image: image, description: desc}
+    // create a new course and save to DB
+    campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.get("/campgrounds/new", function(req, res){ 
     res.render("new");
+});
+
+app.get("/campgrounds/:id", function(req, res){
+    // find course with provided id
+    campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            // render show templte with that course
+            res.render("show", {campground: foundCampground});
+        }
+    });
 });
 
 app.listen(3000, function() { 
